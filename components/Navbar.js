89a7a4d35893208ef9b2d5c073a1ddd30d72dbf9
Cecilia Blankson Amponsah <amponsahcecilia77@gmail.com>
2022-10-13 17:57:16 +0000

    fix anchor link in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -45,22 +45,20 @@ const Navbar = () => {
           >
             <ul className="flex flex-col mt-4 bg-gray-50 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
               <li>
-                <a
+                <Link
                   href="/"
                   className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
                   aria-current="page"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <Link href="/plastic-dealers">
-                  <a
-                    href="plastic-dealers"
-                    className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Plastic Dealers
-                  </a>
+                <Link
+                  href="/plastic-dealers"
+                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  <a>Plastic Dealers</a>
                 </Link>
               </li>
               <li>
